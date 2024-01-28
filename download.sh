a91cd4fc6206cf78320952a4200ec16a888bbea3
Luka Simić <git@kocka.tech>
2024-01-28 17:46:24 +0100

    Try running on GitHub Actions instead.

--- download.sh
+++ download.sh
@@ -5,13 +5,11 @@ cd download
 wget -q $(curl -X POST -d '{"forceDownload": true}' -H "Content-Type: application/json" https://gamejolt.com/site-api/web/discover/games/builds/get-download-url/1501877 | jq -r .payload.url)
 unzip -p undertale-yellow-v1.1.zip "Undertale Yellow v1_1/data.win" > data.win
 git clone https://github.com/krzys-h/UndertaleModTool.git
-wget -q https://github.com/krzys-h/UndertaleModTool/releases/download/bleeding-edge/CLI-ubuntu-latest-Release-isBundled-true.zip
-unzip -q CLI-ubuntu-latest-Release-isBundled-true.zip
 if [ ! -z $NETLIFY ]
 then
-    sudo apt-get install libgdiplus
+    apt-get install libgdiplus
 fi
-./UndertaleModCli load data.win --scripts UndertaleModTool/UndertaleModTool/Scripts/Resource\ Unpackers/ExportAllCode.csx
+../utmtcli/UndertaleModCli load data.win --scripts UndertaleModTool/UndertaleModTool/Scripts/Resource\ Unpackers/ExportAllCode.csx
 mv Export_Code ../decompiled
 cd ..
 rm -rf download
