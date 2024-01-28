db376bc883baf440be761be4a0f7d29052b078f7
Luka Simić <git@kocka.tech>
2024-01-28 17:04:08 +0100

    Make Netlify install libgdiplus.

--- download.sh
+++ download.sh
@@ -2,11 +2,15 @@
 rm -rf decompiled
 mkdir download
 cd download
-wget $(curl -X POST -d '{"forceDownload": true}' -H "Content-Type: application/json" https://gamejolt.com/site-api/web/discover/games/builds/get-download-url/1501877 | jq -r .payload.url)
+wget -q $(curl -X POST -d '{"forceDownload": true}' -H "Content-Type: application/json" https://gamejolt.com/site-api/web/discover/games/builds/get-download-url/1501877 | jq -r .payload.url)
 unzip -p undertale-yellow-v1.1.zip "Undertale Yellow v1_1/data.win" > data.win
 git clone https://github.com/krzys-h/UndertaleModTool.git
-wget https://github.com/krzys-h/UndertaleModTool/releases/download/bleeding-edge/CLI-ubuntu-latest-Release-isBundled-true.zip
-unzip CLI-ubuntu-latest-Release-isBundled-true.zip
+wget -q https://github.com/krzys-h/UndertaleModTool/releases/download/bleeding-edge/CLI-ubuntu-latest-Release-isBundled-true.zip
+unzip -q CLI-ubuntu-latest-Release-isBundled-true.zip
+if [ ! -z $NETLIFY ]
+then
+    sudo apt-get install libgdiplus
+fi
 ./UndertaleModCli load data.win --scripts UndertaleModTool/UndertaleModTool/Scripts/Resource\ Unpackers/ExportAllCode.csx
 mv Export_Code ../decompiled
 cd ..
